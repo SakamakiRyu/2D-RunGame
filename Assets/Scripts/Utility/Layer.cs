52248e62f6823162d13e25a6811c25e35d71feb1
SakamakiRyu <potesara031w@gmail.com>
2021-12-07 15:49:47 +0900

    不要アセットの削除をした

--- Assets/Scripts/Utility/Layer.cs
+++
@@ -1,4 +0,0 @@
-public static class Layer
-{
-    public const int LAYER_GROUND_INDEX = 6;
-}
--- Assets/Scripts/Utility/Tags.cs
+++
@@ -1,9 +0,0 @@
-/// <summary>
-/// “ü—Íƒ~ƒX–h~‚Ìˆ×‚Ìì¬
-/// </summary>
-public static class Tags
-{
-    public const string TAG_ENEMY = "Enemy";
-    public const string TAG_PLAYER = "Player";
-    public const string TAG_GROUND = "Ground";
-}
